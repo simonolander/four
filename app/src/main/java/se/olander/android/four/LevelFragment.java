f06b868baf4335d73fed00a41084f86206c93583
Simon Olander <simon.olander@r2m.se>
2017-12-26 23:01:19 +0100

    fab

--- app/src/main/java/se/olander/android/four/LevelFragment.java
+++ app/src/main/java/se/olander/android/four/LevelFragment.java
@@ -2,7 +2,9 @@ package se.olander.android.four;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,15 @@ public class LevelFragment extends Fragment {
 
         PaintingView painting = view.findViewById(R.id.painting);
         painting.setPainting(Painting.somePainting());
+
+        FloatingActionButton color1 = view.findViewById(R.id.color_one);
+        color1.setOnClickListener(new View.OnClickListener() {
+            private final String TAG = LevelFragment.class.getSimpleName();
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: " + view);
+            }
+        });
 
 
         return view;
--- app/src/main/java/se/olander/android/four/MainActivity.java
+++ app/src/main/java/se/olander/android/four/MainActivity.java
@@ -2,12 +2,16 @@ package se.olander.android.four;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate theme: " + getTheme());
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager()
             .beginTransaction()
